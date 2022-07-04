0f1bc549f3941eab262dd69722edb0b010ee1435
Nauruz <nz.agoev@gmail.com>
2022-07-04 19:25:55 +0300

    fixed date picker

---
+++ src/test/java/tests/RegistrationFormTests.java
@@ -0,0 +1,59 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+
+public class RegistrationFormTests extends TestBase {
+
+
+    @Test
+    void successfulTest() {
+        String firstName = "Neo";
+        String lastName = "Rus";
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#genterWrapper").$(byText("Other")).click();
+ //     $("#gender-radio-3").parent().click();
+        $("#userNumber").setValue("0123456789");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+
+        $("#subjectsInput").sendKeys("Maths");
+        $("#subjectsInput").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+//        $("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
+        $("#currentAddress").setValue("Some street 1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+//        $(".table-responsive").shouldHave(text(firstName), text(lastName), text("alex@egorov.com"),
+//            text("30 May,2000"), text("Some street 1"));
+
+//        $(".table-responsive").$(byText()"Date of Birth"))
+//                .parent().shouldHave(text("30 May,2000"));
+        checkTable("Date of Birth", "30 May,2000");
+    }
+
+    void checkTable(String key, String value) {
+        $(".table-responsive").$(byText(key)).parent().shouldHave(text(value));
+    }
+}
---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTests extends TestBase {
+    @Test
+    void successfulTest() {
+        String name = "Alex";
+
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("[id=userName]").setValue(name);
+        $("[id=userEmail]").setValue("alex@egorov.com");
+        $("[id=currentAddress]").setValue("Some address 1");
+        $("[id=permanentAddress]").setValue("Another address 2");
+        $("[id=submit]").click();
+
+        $("[id=output]").shouldHave(text(name), text("alex@egorov.com"),
+                text("Some address 1"), text("Another address 2"));
+    }
+}
