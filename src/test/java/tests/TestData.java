83f9d8eb00a73375c7a82ca661a8b2cbfd821c98
Nauruz <nz.agoev@gmail.com>
2022-08-23 12:46:44 +0300

    fixed address and checkresults

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -9,6 +9,7 @@ public class TestData {
     static String firstName = faker.name().firstName();
     static String lastName = faker.name().lastName();
     static String userEmail = faker.internet().emailAddress();
+    //static String userAdress = String.valueOf(faker.address());
     static String userAdress = String.valueOf(faker.address());
     static String userNumber = String.valueOf(faker.number().randomNumber(10, true));
 
