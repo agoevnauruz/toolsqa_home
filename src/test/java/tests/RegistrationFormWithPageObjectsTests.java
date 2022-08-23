745fb26a568f9ef329c612f8dfb063a75b60e405
Nauruz <nz.agoev@gmail.com>
2022-08-23 12:10:02 +0300

    change value

--- src/test/java/tests/RegistrationFormWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationFormWithPageObjectsTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static tests.TestData.*;
-import static tests.TestData.userAdress;
 
 
 public class RegistrationFormWithPageObjectsTests extends TestBase {
@@ -31,13 +30,6 @@ public class RegistrationFormWithPageObjectsTests extends TestBase {
                 .checkResult("Student Name", firstName )
                 .checkResult("Student Name", lastName)
                 .checkResult("Student Email", userEmail )
-                .checkResult("Gender", "Other" )
-                .checkResult("Mobile", userNumber )
-                .checkResult("Date of Birth", "30 May,2000")
-                .checkResult("Subjects", "Maths")
-                .checkResult("Hobbies", "Sports")
-                .checkResult("Picture", "1.png")
-                .checkResult("Address", "Some street 1")
-                .checkResult("State and City", "NCR" + "Delhi");
+                .checkResult("Date of Birth", "30 May,2000");
     }
 }
